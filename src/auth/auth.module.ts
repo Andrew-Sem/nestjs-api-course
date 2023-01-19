af4fcc9322783c0fdf131c599b2e4896e3103c14
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-20 00:13:40 +0300

    simple controllers and service logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,9 @@
-import { Module } from "@nestjs/common";
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common';
 
-@Module({})
-export class AuthModule{}
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
